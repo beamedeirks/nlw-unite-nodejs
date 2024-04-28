c1a66f641383ff614a7330120ce3cf12dbeb9d35
Beatriz <beatriz.lhopes@alcans.com.br>
2024-04-28 10:54:24 -0300

    Projeto atualizado

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler} from "fastify-type-provider-zod"
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
-
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 
@@ -11,8 +14,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
-
-app.listen({ port: 3333 }).then(() =>{
-  console.log('Http server runing!')
+app.listen({ port: 3333 }).then(() => {
+  console.log("Http server runing!");
 });
